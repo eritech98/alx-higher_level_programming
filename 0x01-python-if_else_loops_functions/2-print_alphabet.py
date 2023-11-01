8a97263b5a8a7b533ce25bf8d96b9baa32e9bf0c
Erick Olando <Opiyoerick1998@gmail.com>
2023-10-31 17:06:22 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 for letter_e in range(97, 123):
     if letter_e == 101 or letter_e == 113:
         continue
-    print("{}".format(chr(letter_e)), end="")
+print("{}".format(chr(letter_e)), end="")

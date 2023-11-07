12b21f33617d32bef91169d640e4ff8739c3548c
Erick Olando <Opiyoerick1998@gmail.com>
2023-11-06 17:13:44 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer').print_list_integer
-    """This will print all the intergers in the list"""
+"""This will print all the intergers in the list"""
 for i in my_list:
     print("{:d}".format(i))

1af666edf0f302d8dda97683e69e5ba412ad53d8
Erick Olando <Opiyoerick1998@gmail.com>
2024-01-17 13:41:41 -0800

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- Creating Database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creating User.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Granting permission.
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa.
+
+-- Listing...
+SELECT cities.id, cities.name
+FROM cities
+WHERE state_id = (
+	SELECT states.id
+	FROM states
+	WHERE states.name = 'California'
+)
+ORDER BY cities.id;
